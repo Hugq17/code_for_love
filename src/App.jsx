db3d748dd5fc7a85af6d6e348d89d82741bb8b85
hung1720 <coderlazy17@gmail.com>
2024-05-11 23:42:35 +0700

    homepage

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Homepage from "./page/Homepage";
 import Dashboard from "./page/Dashboard";
 import { useState } from "react";
 import PrivateRoute from "./component/PrivateRoute";
+import Homepage1 from "./page/Homepage1";
+import Homepage2 from "./page/Homepage2";
 function App() {
   const [isloggIn, setIsLoggIn] = useState(false);
 
@@ -18,6 +20,8 @@ function App() {
           element={<Login updateStatus={updateStatus} />}
         ></Route>
         <Route path="/" element={<Homepage />}></Route>
+        <Route path="/homepage1" element={<Homepage1 />}></Route>
+        <Route path="/homepage2" element={<Homepage2 />}></Route>
         <Route
           path="/dashboard"
           element={
--- src/page/Homepage1.jsx
+++ src/page/Homepage1.jsx
@@ -4,19 +4,19 @@ import "../styles/Homepage.css";
 import { Player } from "@lottiefiles/react-lottie-player";
 import run from "../assets/lotties/run.json";
 
-const Homepage = () => {
+const Homepage1 = () => {
   const navigate = useNavigate();
   const goToLogin = () => {
-    navigate("/homepage1");
+    navigate("/homepage2");
   };
   return (
-    <div className="background-image relative">
-      <p className="text-white text-3xl">2022</p>
-      <button onClick={goToLogin} className="absolute bottom-0 right-32 w-full">
+    <div className="background-image1 relative">
+      <p className="text-white text-3xl">2023</p>
+      <button onClick={goToLogin} className="absolute bottom-0 right-0 w-full">
         <Player autoplay loop src={run} className="w-2/3"></Player>
       </button>
     </div>
   );
 };
 
-export default Homepage;
+export default Homepage1;
